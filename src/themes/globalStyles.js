71acdac29e4146c5cb17ca768fce4a996bfa3ad1
Huyaoko12 <huylooc12@gmail.com>
2024-09-26 11:22:46 +0700

    create branch

---
+++ src/themes/globalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        font-size: 50%;
+    }
+
+    body {
+        font-family: "Roboto Slab", serif;
+        font-size: 1.6rem;
+        line-height: 1.5;
+    }
+`;
+
+export default GlobalStyles;
---
+++ src/themes/index.js
@@ -0,0 +1,7 @@
+const PRIMARY_COLOR = '#FFFFFF'; 
+
+export const theme = {
+    colors: {
+        primary: PRIMARY_COLOR,
+    }
+}

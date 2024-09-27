392c193a04e36d5e0336068be51d0cd80e30d513
Phan Long <longbp166@gmail.com>
2024-09-27 15:32:16 +0700

    style: 💄 add style & color

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,8 +1,14 @@
-const PRIMARY_COLOR = '#FFFFFF'; 
-
 export const theme = {
     colors: {
-        primary: PRIMARY_COLOR,
         white: '#FFF',
+        black: '#000',
+        discord: 'rgb(87, 101, 242)',
+        footer: 'rgb(20, 20, 20)',
+        textNavigation: 'rgb(240, 220, 192)',
+        navDropdown: 'rgb(199, 100, 61)',
+        shadowNav: 'rgb(114, 64, 81)'
+    },
+    transition: {
+        primary: 'all linear 0.25s',
     }
 }
